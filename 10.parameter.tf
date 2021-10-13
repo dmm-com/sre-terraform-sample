85d6224411acc5b9b651c6a8d105e419c9256373
nakano-tomohiko <nakano-tomohiko@kcgrp.com>
2021-10-13 20:53:59 +0900

    fix

--- 10.parameter.tf
+++ 10.parameter.tf
@@ -5,25 +5,25 @@ resource "random_password" "master_password" {
 }
 
 resource "aws_ssm_parameter" "rds_password" {
-  name   = "/rds/password"
+  name   = "/rds/API_DBPASS"
   type   = "String"
   value  = random_password.master_password.result
 }
 
 resource "aws_ssm_parameter" "rds_dbname" {
-  name   = "/rds/dbname"
+  name   = "/rds/API_DBNAME"
   type   = "String"
   value  = var.database_name
 }
 
 resource "aws_ssm_parameter" "rds_user_name" {
-  name   = "/rds/user"
+  name   = "/rds/API_DBUSER"
   type   = "String"
   value  = var.master_username
 }
 
 resource "aws_ssm_parameter" "rds_endpoint" {
-  name   = "/rds/endpoint"
+  name   = "/rds/API_DBHOST"
   type   = "String"
   value  = aws_rds_cluster.rds.endpoint
 }
